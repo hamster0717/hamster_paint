871ba3109d9f9aa0d86a59d389149086f7bc7ff5
hamster <aoscar0717@gmail.com>
2024-10-05 22:15:16 +0800

    加入專案檔案。

---
+++ hamster_paint/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace hamster_paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Save = new System.Windows.Forms.Button();
+            this.circle = new System.Windows.Forms.RadioButton();
+            this.line = new System.Windows.Forms.RadioButton();
+            this.square = new System.Windows.Forms.RadioButton();
+            this.prebtn = new System.Windows.Forms.Button();
+            this.nexbtn = new System.Windows.Forms.Button();
+            this.openbtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::hamster_paint.Properties.Resources.sad;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1024, 768);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            this.pictureBox1.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseWheel);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(12, 12);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 40);
+            this.Save.TabIndex = 1;
+            this.Save.Text = "储存";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // circle
+            // 
+            this.circle.AutoSize = true;
+            this.circle.Location = new System.Drawing.Point(469, 21);
+            this.circle.Name = "circle";
+            this.circle.Size = new System.Drawing.Size(73, 22);
+            this.circle.TabIndex = 2;
+            this.circle.TabStop = true;
+            this.circle.Text = "circle";
+            this.circle.UseVisualStyleBackColor = true;
+            this.circle.CheckedChanged += new System.EventHandler(this.TypeChange);
+            // 
+            // line
+            // 
+            this.line.AutoSize = true;
+            this.line.Location = new System.Drawing.Point(404, 21);
+            this.line.Name = "line";
+            this.line.Size = new System.Drawing.Size(59, 22);
+            this.line.TabIndex = 3;
+            this.line.TabStop = true;
+            this.line.Text = "line";
+            this.line.UseVisualStyleBackColor = true;
+            this.line.CheckedChanged += new System.EventHandler(this.TypeChange);
+            // 
+            // square
+            // 
+            this.square.AutoSize = true;
+            this.square.Location = new System.Drawing.Point(548, 21);
+            this.square.Name = "square";
+            this.square.Size = new System.Drawing.Size(78, 22);
+            this.square.TabIndex = 4;
+            this.square.TabStop = true;
+            this.square.Text = "square";
+            this.square.UseVisualStyleBackColor = true;
+            this.square.CheckedChanged += new System.EventHandler(this.TypeChange);
+            // 
+            // prebtn
+            // 
+            this.prebtn.Location = new System.Drawing.Point(174, 12);
+            this.prebtn.Name = "prebtn";
+            this.prebtn.Size = new System.Drawing.Size(75, 40);
+            this.prebtn.TabIndex = 5;
+            this.prebtn.Text = "上一步";
+            this.prebtn.UseVisualStyleBackColor = true;
+            this.prebtn.Click += new System.EventHandler(this.prebtn_Click);
+            // 
+            // nexbtn
+            // 
+            this.nexbtn.Location = new System.Drawing.Point(255, 12);
+            this.nexbtn.Name = "nexbtn";
+            this.nexbtn.Size = new System.Drawing.Size(75, 40);
+            this.nexbtn.TabIndex = 6;
+            this.nexbtn.Text = "下一步";
+            this.nexbtn.UseVisualStyleBackColor = true;
+            this.nexbtn.Click += new System.EventHandler(this.nexbtn_Click);
+            // 
+            // openbtn
+            // 
+            this.openbtn.Location = new System.Drawing.Point(93, 12);
+            this.openbtn.Name = "openbtn";
+            this.openbtn.Size = new System.Drawing.Size(75, 40);
+            this.openbtn.TabIndex = 7;
+            this.openbtn.Text = "打開";
+            this.openbtn.UseVisualStyleBackColor = true;
+            this.openbtn.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1050, 831);
+            this.Controls.Add(this.openbtn);
+            this.Controls.Add(this.nexbtn);
+            this.Controls.Add(this.prebtn);
+            this.Controls.Add(this.square);
+            this.Controls.Add(this.line);
+            this.Controls.Add(this.circle);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.RadioButton circle;
+        private System.Windows.Forms.RadioButton line;
+        private System.Windows.Forms.RadioButton square;
+        private System.Windows.Forms.Button prebtn;
+        private System.Windows.Forms.Button nexbtn;
+        private System.Windows.Forms.Button openbtn;
+    }
+}
+
---
+++ hamster_paint/Form1.cs
@@ -0,0 +1,249 @@
+﻿using OpenCvSharp.Extensions;
+using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection.Emit;
+using System.Reflection;
+using OpenCvSharp.Dnn;
+
+namespace hamster_paint
+{
+    public partial class Form1 : Form
+    {
+        private Stack<Mat> pre=new Stack<Mat>();
+        private Stack<Mat> nex = new Stack<Mat>();
+        private Mat canvas;
+        private Mat tempCanvas;
+        private OpenCvSharp.Point previousPoint;
+        private OpenCvSharp.Point startPoint;
+        private bool isDrawing = false;
+        private int drawingType = 0;
+        private int zoomstep = 30;
+        private OpenCvSharp.Point dragStartPoint;
+        public Form1()
+        {
+            InitializeComponent();
+            canvas = new Mat(new OpenCvSharp.Size(pictureBox1.Width, pictureBox1.Height), MatType.CV_8UC3, Scalar.White);
+            tempCanvas = canvas.Clone();
+            pictureBox1.Image = BitmapConverter.ToBitmap(canvas);
+
+        }
+
+        
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine(canvas.Width + " " + pictureBox1.Width);
+            if (e.Button == MouseButtons.Left)
+            {
+                if (canvas != null)
+                    pre.Push(canvas.Clone());
+                isDrawing = true;
+                double scaleX = (double)canvas.Width / pictureBox1.Width;
+                double scaleY = (double)canvas.Height / pictureBox1.Height;
+                startPoint = new OpenCvSharp.Point(e.X * scaleX, e.Y * scaleY);
+                previousPoint = startPoint;
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                isDrawing = true;
+                dragStartPoint.X = Cursor.Position.X;
+                dragStartPoint.Y = Cursor.Position.Y;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing && e.Button == MouseButtons.Left)
+            {
+                double scaleX = (double)canvas.Width / pictureBox1.Width;
+                double scaleY = (double)canvas.Height / pictureBox1.Height;
+                OpenCvSharp.Point currentPoint = new OpenCvSharp.Point(e.X * scaleX, e.Y * scaleY);
+                tempCanvas = canvas.Clone();
+                switch (drawingType)
+                {
+                    case 0:
+                        Cv2.Line(canvas, previousPoint, currentPoint, Scalar.Black, 2);
+                        previousPoint = currentPoint;
+                        break;
+                    case 1:
+                        DrawEllipse(tempCanvas, startPoint, currentPoint);
+                        break;
+                    case 2:
+                        DrawRectangle(tempCanvas, startPoint, currentPoint);
+                        break;
+                    default:
+                        return;
+                }
+                pictureBox1.Image = BitmapConverter.ToBitmap(tempCanvas);
+            }
+            else if (e.Button == MouseButtons.Right&&isDrawing)
+            {
+
+                int x, y;
+                int moveX, moveY;
+                moveX = Cursor.Position.X - dragStartPoint.X;
+                moveY = Cursor.Position.Y - dragStartPoint.Y;
+                x = pictureBox1.Location.X + moveX;
+                y = pictureBox1.Location.Y + moveY;
+                pictureBox1.Location = new System.Drawing.Point(x, y);
+                dragStartPoint.X = Cursor.Position.X;
+                dragStartPoint.Y = Cursor.Position.Y;
+            }
+        }
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                double scaleX = (double)canvas.Width / pictureBox1.Width;
+                double scaleY = (double)canvas.Height / pictureBox1.Height;
+                OpenCvSharp.Point currentPoint = new OpenCvSharp.Point(e.X * scaleX, e.Y * scaleY);
+                switch (drawingType)
+                {
+                    case 0:
+                        Cv2.Line(canvas, previousPoint, currentPoint, Scalar.Black, 2);
+                        previousPoint = currentPoint;
+                        break;
+                    case 1:
+                        DrawEllipse(canvas, startPoint, currentPoint);
+                        break;
+                    case 2:
+                        DrawRectangle(canvas, startPoint, currentPoint);
+                        break;
+                    default:
+                        break;
+                }
+                pictureBox1.Image = BitmapConverter.ToBitmap(canvas);
+                isDrawing = false;
+
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                isDrawing = false;
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif|Png Image|*.png";
+                saveFileDialog.Title = "储存圖片";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = saveFileDialog.FileName;
+                    Cv2.ImWrite(filePath, canvas);
+                }
+            }
+        }
+        private void Open_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif|Png Image|*.png";
+                openFileDialog.Title = "打開圖片";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+                    //Console.WriteLine(filePath);
+                    Mat image = Cv2.ImRead(filePath);
+                    canvas = image.Clone();
+                    tempCanvas = canvas.Clone();
+                    pictureBox1.Image = BitmapConverter.ToBitmap(canvas);
+                }
+            }
+        }
+        private void DrawEllipse(Mat img, OpenCvSharp.Point start, OpenCvSharp.Point end)
+        {
+            int centerX = (start.X + end.X) / 2;
+            int centerY = (start.Y + end.Y) / 2;
+            int axisX = Math.Abs(start.X - end.X) / 2;
+            int axisY = Math.Abs(start.Y - end.Y) / 2;
+            Cv2.Ellipse(img, new OpenCvSharp.Point(centerX, centerY), new OpenCvSharp.Size(axisX, axisY), 0, 0, 360, Scalar.Black, 2);
+        }
+        private void DrawRectangle(Mat img, OpenCvSharp.Point start, OpenCvSharp.Point end)
+        {
+            Cv2.Rectangle(img, start, end, Scalar.Black, 2, LineTypes.Link8, 0);
+        }
+        private readonly Dictionary<String, int> TypeToInt = new Dictionary<String, int>
+        {
+            {"line",0},{"circle",1},{"square",2}
+        };
+
+        private void TypeChange(object sender, EventArgs e)
+        {
+            RadioButton val= (RadioButton)sender;
+            if (val.Checked)
+            {
+                drawingType = TypeToInt[val.Text];
+            }
+        }
+        private void pictureBox1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            int x = e.Location.X;
+            int y = e.Location.Y;
+            int ow = pictureBox1.Width;
+            int oh = pictureBox1.Height;
+            int VX, VY;
+
+            PropertyInfo pInfo = pictureBox1.GetType().GetProperty("ImageRectangle", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (pInfo == null)
+            {
+                return;
+            }
+
+            if (e.Delta > 0)
+            {
+                pictureBox1.Width += zoomstep;
+                pictureBox1.Height += zoomstep;
+            }
+            else if (e.Delta < 0)
+            {
+                if (pictureBox1.Width < canvas.Width / 10)
+                    return;
+
+                pictureBox1.Width -= zoomstep;
+                pictureBox1.Height -= zoomstep;
+            }
+
+            Rectangle rect = (Rectangle)pInfo.GetValue(pictureBox1, null);
+            pictureBox1.Width = rect.Width;
+            pictureBox1.Height = rect.Height;
+            VX = (int)((double)x * (ow - pictureBox1.Width) / ow);
+            VY = (int)((double)y * (oh - pictureBox1.Height) / oh);
+            pictureBox1.Location = new System.Drawing.Point(pictureBox1.Location.X + VX, pictureBox1.Location.Y + VY);
+
+        }
+
+        private void prebtn_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(pre.Count());
+            if (pre.Count() != 0)
+            {
+                nex.Push(canvas.Clone());
+                tempCanvas = canvas = pre.Pop();
+            }
+                
+            pictureBox1.Image = BitmapConverter.ToBitmap(canvas);
+        }
+
+        private void nexbtn_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(nex.Count());
+            if (nex.Count() != 0)
+            {
+                pre.Push(canvas.Clone());
+                tempCanvas = canvas = nex.Pop();
+            }
+
+            pictureBox1.Image = BitmapConverter.ToBitmap(canvas);
+        }
+    }
+}
